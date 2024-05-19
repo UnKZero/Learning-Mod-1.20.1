7e1ab3ea452145d54f9dc917058e3c4d950e1fb5
UnKZero <103445831+UnKZero@users.noreply.github.com>
2024-05-19 20:20:39 +0300

    Commit

--- src/main/java/net/unkzero/learningmod/LearningMod.java
+++ src/main/java/net/unkzero/learningmod/LearningMod.java
@@ -17,7 +17,7 @@ public class LearningMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		// m
+
 
 
 		LOGGER.info("Hello Fabric world!");
