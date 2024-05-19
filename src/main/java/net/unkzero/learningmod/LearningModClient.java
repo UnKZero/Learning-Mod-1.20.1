6f119b29307bcbc20597675780d8a2b5ebb9e131
UnKZero <cybernaticzero@gmail.com>
2024-05-19 19:42:26 +0300

    E

---
+++ src/main/java/net/unkzero/learningmod/LearningModClient.java
@@ -0,0 +1,11 @@
+package net.unkzero.learningmod;
+
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LearningModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
